b2c4e9fd2acb834fc14e75c2ee36e56d4676df71
Ubuntu <ubuntu@ip-172-26-14-126.ap-south-1.compute.internal>
2023-06-15 18:02:28 +0000

    renaming

---
+++ staticfiles/js/login.js
@@ -0,0 +1,8 @@
+// [...document.getElementsByClassName("input-box")].forEach(element => {
+//     element.addEventListener("click", e => {
+//         e.preventDefault();
+//         if(!element.classList.contains('active')){
+//             element.classList.add('active');
+//         }
+//     })
+// });
